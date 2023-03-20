0a5a048a8334dc84fcf4209b6838213f8790d008
Daniel Mendes <contact@daniel-mendes.ch>
2023-03-20 21:21:35 +0100

    ux: Separate raining interaction of cloud left and right

--- assets/js/hydraulic_raining.js
+++ assets/js/hydraulic_raining.js
@@ -11,15 +11,25 @@ gsap.to(".rain1 , .rain2, .rain3, .rain4", {
     ease: "none"
 });
 
-const rainingContainer = document.querySelector("#raining-container");
+
 const clouds = document.querySelectorAll('.cloud');
+const rainingLeft = document.querySelector(".raining-left");
+const rainingRight = document.querySelector(".raining-right");
 
 clouds.forEach((cloud) => {
     onClassChange(cloud, (element) => {
         if (element.classList.contains('speed-up')) {
-            rainingContainer.style.visibility = "visible";
+            if (element.classList.contains('cloud-left')) {
+                rainingLeft.style.visibility = "visible";
+            } else {
+                rainingRight.style.visibility = "visible";
+            }
         } else {
-            rainingContainer.style.visibility = "hidden";
+            if (element.classList.contains('cloud-left')) {
+                rainingLeft.style.visibility = "hidden";
+            } else {
+                rainingRight.style.visibility = "hidden";
+            }
         }
     });
 });
