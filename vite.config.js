bfaa0640406a045b77ef99435fb15fc2250a63b0
Daniel Mendes <contact@daniel-mendes.ch>
2023-03-23 11:38:39 +0100

    fix: Fixed json assets not copied to prod

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default {
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/data/owid-energy-switzerland-data.json',
+          dest: 'assets/'
+        }
+      ]
+    })
+  ]
+}
