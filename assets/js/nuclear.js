bb5e3f5ef067bec97ac782598f855998fd4a698c
Daniel Mendes <contact@daniel-mendes.ch>
2023-04-23 23:13:01 +0200

    refactor: Refactor nuclear smoke

--- assets/js/nuclear.js
+++ assets/js/nuclear.js
@@ -1,32 +1,34 @@
 import { gsap, Power1 } from "gsap";
 import { onClassChange } from "./utils.js";
 
-const bubblesTimeline = gsap.fromTo('#bubbleGroup > *',
-  {
-    x: 'random(-25, 25)',
-    y: 0,
-    scale: 'random(0.1, 1)',
-    transformOrigin: "50% 50%"
-  }, { 
-    y: -1000, 
-    stagger: {
-      repeatRefresh: true,
-      each: 0.8,
-      repeat: -1
+const bubblesTimeline = gsap
+  .fromTo(
+    "#bubbleGroup > *",
+    {
+      x: 0,
+      y: 0,
+      scale: "random(0.1, 0.6)",
     },
-    transformOrigin: "50% 50%",
-    duration: 'random(30, 50)',
-    ease: Power1.easeOut,
-    x: 'random(-350, 300)'
-  }
-).seek(1000);
+    {
+      y: -1000,
+      stagger: {
+        repeatRefresh: true,
+        each: 0.8,
+        repeat: -1,
+      },
+      duration: "random(50, 80)",
+      ease: Power1.easeOut,
+      x: "random(-350, 300)",
+    }
+  )
+  .seek(1000);
 
-const nuclearReactor = document.querySelector('#nuclear #nuclear-reactor');
+const nuclearReactor = document.querySelector("#nuclear #nuclear-reactor");
 
 onClassChange(nuclearReactor, (element) => {
-    if (element.classList.contains('speed-up')) {
-        bubblesTimeline.timeScale(5);
-    } else {
-        bubblesTimeline.timeScale(1);
-    }
+  if (element.classList.contains("speed-up")) {
+    bubblesTimeline.timeScale(5);
+  } else {
+    bubblesTimeline.timeScale(1);
+  }
 });
--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ import "./navigation.js";
 import "./interactive.js";
 import "./charts.js";
 import "./fossil.js";
+
 import "./nuclear.js";
 import "./wind.js";
 import "./maps.js";
