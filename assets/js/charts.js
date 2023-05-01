f75b110cd2e06539fab0e26a68c780c45eb67438
Daniel Mendes <contact@daniel-mendes.ch>
2023-05-01 06:56:09 +0200

    wip: Working on hydraulic chart

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,5 +1,11 @@
 import * as d3 from "d3";
-import { nuclearColor, fossilColor, hydraulicColor } from "./colors";
+import {
+  nuclearColor,
+  fossilColor,
+  hydraulicColor,
+  solarColor,
+  windColor,
+} from "./colors";
 
 d3.json("assets/data/owid-energy-switzerland-data.json")
   .then((data) => {
@@ -23,7 +29,11 @@ d3.json("assets/data/owid-energy-switzerland-data.json")
 
         dataParsed.hydraulicChart.push({
           year: d3.timeParse("%Y")(d.year),
+          fossil_electricity_production: d.fossil_electricity || 0,
           hydraulic_electricity_production: d.hydro_electricity || 0,
+          nuclear_electricity_production: d.nuclear_electricity || 0,
+          solar_electricity_production: d.solar_electricity || 0,
+          wind_electricity_production: d.wind_electricity || 0,
         });
       }
     });
@@ -218,6 +228,8 @@ const fossilChart = (data) => {
 };
 
 const hydraulicChart = (data) => {
+  // Stacked area chart
+
   const margin = { top: 40, right: 40, bottom: 40, left: 80 },
     width = 1024 - margin.left - margin.right,
     height = 512 - margin.top - margin.bottom;
@@ -234,14 +246,31 @@ const hydraulicChart = (data) => {
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  // Title
-  svg
-    .append("text")
-    .attr("x", width / 2)
-    .attr("y", -margin.top / 2)
-    .attr("text-anchor", "middle")
-    .style("font-size", 16)
-    .text("Production d'énergie hydraulique");
+  const keys = [
+    "hydraulic_electricity_production",
+    "fossil_electricity_production",
+    "nuclear_electricity_production",
+    "solar_electricity_production",
+    "wind_electricity_production",
+  ];
+
+  const keysToColors = {
+    hydraulic_electricity_production: hydraulicColor,
+    fossil_electricity_production: fossilColor,
+    nuclear_electricity_production: nuclearColor,
+    solar_electricity_production: solarColor,
+    wind_electricity_production: windColor,
+  };
+
+  const keysToLabels = {
+    hydraulic_electricity_production: "Hydraulique",
+    fossil_electricity_production: "Fossile",
+    nuclear_electricity_production: "Nucléaire",
+    solar_electricity_production: "Solaire",
+    wind_electricity_production: "Eolien",
+  };
+
+  const stackedData = d3.stack().keys(keys)(data);
 
   const x = d3
     .scaleTime()
@@ -250,43 +279,50 @@ const hydraulicChart = (data) => {
 
   const y = d3
     .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.hydraulic_electricity_production)])
+    .domain([0, d3.max(stackedData[stackedData.length - 1], (d) => d[1])])
     .range([height, 0]);
+
+  const clip = svg
+    .append("defs")
+    .append("svg:clipPath")
+    .attr("id", "clip")
+    .append("svg:rect")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("x", 0)
+    .attr("y", 0);
+
+  const areaChart = svg.append("g").attr("clip-path", "url(#clip)");
 
   const area = d3
     .area()
-    .x((d) => x(d.year))
-    .y0(height)
-    .y1((d) => y(d.hydraulic_electricity_production));
+    .x((d) => x(d.data.year))
+    .y0((d) => y(d[0]))
+    .y1((d) => y(d[1]));
 
-  const line = d3
-    .line()
-    .x((d) => x(d.year))
-    .y((d) => y(d.hydraulic_electricity_production));
+  // Highlight hover effect
+  const highlight = (d) => {
+    // Reduce opacity of all the areas
+    d3.selectAll(".area").style("opacity", 0.1);
+    // expect the one that is hovered
+    d3.select(`.${d.currentTarget.classList[1]}`).style("opacity", 1);
+  };
 
-  const path = svg
+  // And when it is not hovered anymore
+  const noHighlight = (event) => {
+    d3.selectAll(".area").style("opacity", 1);
+  };
+
+  areaChart
+    .selectAll(".layer")
+    .data(stackedData)
+    .enter()
     .append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", hydraulicColor)
-    .attr("stroke-linejoin", "round")
-    .attr("stroke-linecap", "round")
-    .attr("stroke-width", 1.5)
-    .attr("d", line);
-
-  svg
-    .append("path")
-    .datum(data)
-    .attr("fill", hydraulicColor)
-    .attr("fill-opacity", 0.3)
-    .attr("stroke", "none")
+    .attr("class", (d) => `area ${keysToLabels[d.key]}`)
+    .style("fill", (d) => {
+      return keysToColors[d.key];
+    })
     .attr("d", area);
-
-  const pathLength = path.node().getTotalLength();
-
-  path
-    .attr("stroke-dashoffset", pathLength)
-    .attr("stroke-dasharray", pathLength);
 
   svg
     .append("g")
@@ -296,17 +332,48 @@ const hydraulicChart = (data) => {
 
   svg.append("g").attr("class", "axis-lines").call(d3.axisLeft(y));
 
-  const xGrid = d3.axisBottom(x).tickSize(-height, 0, 0).tickFormat("");
-
-  const yGrid = d3.axisLeft(y).tickSize(-width, 0, 0).tickFormat("");
+  // Add a legend for each source
+  const size = 20;
+  svg
+    .selectAll(".areaRectangles")
+    .data(keys)
+    .enter()
+    .append("rect")
+    .attr("x", width - 100)
+    .attr("y", (d, i) => 10 + i * (size + 5))
+    .attr("width", 10)
+    .attr("height", 10)
+    .attr("fill", (d) => keysToColors[d])
+    .attr("class", (d) => `area ${keysToLabels[d]}`)
+    .on("mouseover", highlight)
+    .on("mouseleave", noHighlight)
+    .on("click", highlightClick);
 
   svg
-    .append("g")
-    .attr("class", "grid-lines")
-    .attr("transform", `translate(0, ${height})`)
-    .call(xGrid);
+    .selectAll(".areaLabels")
+    .data(keys)
+    .enter()
+    .append("text")
+    .attr("x", width - 100 + size * 1.2)
+    .attr("y", (d, i) => 10 + i * (size + 5) + size / 2)
+    .attr("text-anchor", "left")
+    .attr("alignment-baseline", "middle")
+    .style("font-size", 10)
+    .attr("class", (d) => `area ${keysToLabels[d]}`)
+    .text((d) => keysToLabels[d])
 
-  svg.append("g").attr("class", "grid-lines").call(yGrid);
+    .on("mouseover", highlight)
+    .on("mouseleave", noHighlight)
+    .on("click", highlightClick);
+
+  // Title
+  svg
+    .append("text")
+    .attr("x", width / 2)
+    .attr("y", -margin.top / 2)
+    .attr("text-anchor", "middle")
+    .style("font-size", 16)
+    .text("Production d'énergie par source");
 
   // Label x-axis
   svg
@@ -323,7 +390,7 @@ const hydraulicChart = (data) => {
     .attr("y", -margin.left / 2)
     .attr("text-anchor", "middle")
     .attr("transform", "rotate(-90)")
-    .text("Production d'électricité hydraulique (TWh)");
+    .text("Production d'électricité (TWh)");
 
   // Label source
   svg
