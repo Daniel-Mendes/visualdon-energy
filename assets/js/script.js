4598f16ee821f125ea170f26376cdad886ef5941
Daniel Mendes <contact@daniel-mendes.ch>
2023-05-03 15:59:15 +0200

    refactor: Change js and css loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-import "../scss/style.scss";
-
 import "./utils.js";
 import "./navigation.js";
 import "./interactive.js";
 import "./charts.js";
-import "./fossil.js";
 
+import "./fossil.js";
 import "./nuclear.js";
 import "./wind.js";
 import "./maps.js";
